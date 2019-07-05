6eb758a674daac779cbd3d8ed6fb95fc7b116acf
vidya <vsagarj@gmail.com>
2019-07-05 00:40:11 -0700

    Delete outdated code.

--- src/sequence/index.js
+++ src/sequence/index.js
@@ -1,9 +1,4 @@
-import React, { useState, useEffect } from 'react';
-
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
+import React from 'react';
 
 import './index.scss';
 
